50f5226fb775b50c9da456b8e8606c386ae4fa9b
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-03-02 11:04:12 -0600

    dist build

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export {default as AboutMe} from "./AboutMe"
-export {default as Contact} from "./Contact"
-export {default as Portfolio} from "./Portfolio"
-export {default as Resume} from "./Resume"
