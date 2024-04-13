024864ec147993d95fb72f415412d788720419e0
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-04-12 20:43:55 -0500

    added robots.txt file

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -9,16 +9,16 @@ function Resume() {
             <h4>Technical Skills</h4>
             <section>
                 <b>Frontend:</b> HTML 5, CSS 3, JavaScript, jQuery, React.js, Handlebars<br />
-                <b>Other Languages:</b> C++<br />
+                <b>Other Languages:</b> C++, Java, C#<br />
                 <b>Backend:</b> MySQL, MongoDB, Express, Node.js<br />
             </section><br />
             <h4>Projects</h4>
-            <b><a href="https://github.com/KDeLaria/stock-portfolio-app" target="_blank">Stock Portfolio</a> | March 13, 2024</b>
+            <b><a href="https://github.com/KDeLaria/stock-portfolio-app" target="_blank">Stock Portfolio</a></b>
             <ul>
                 <li>• Displays news articles about businesses.</li>
                 <li>• Tools/Languages:  React, Tailwind, CSS, HTML, JavaScript</li>
             </ul><br />
-            <b><a href="https://github.com/ag-bootcamp-umn/pets-r-us" target="_blank">Pets R Us</a> | February 10, 2024</b>
+            <b><a href="https://github.com/ag-bootcamp-umn/pets-r-us" target="_blank">Pets R Us</a></b>
             <ul>
                 <li>• Allows the user to select a date and time to meet and adopt a selected pet.</li>
                 <li>• Tools/Languages:  Handlebars, Bootstrap, CSS, HTML, JavaScript</li>
